ee7e26602174140f724723e3681b2ae09b0f8f60
Varun <s.varun1054@gmail.com>
2019-03-12 16:13:02 +0530

    initial html css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <textarea autoFocus  defaultValue='' className="story" name="story"/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
